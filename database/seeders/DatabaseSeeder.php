80464196e7594136a8ef07d4b642e1e2699699ce
reancirl <reancirl@gmail.com>
2022-09-03 20:28:10 +0800

    fix seeder issue

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
 
-        \App\Models\User::factory()->create([
+        \App\Models\User::create([
             'id_number' => 'admin',
             'email' => 'admin@admin.com',
             'role' => 'admin',
